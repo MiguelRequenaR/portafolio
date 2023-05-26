6efe598d539b54ed964302c0152f7ff06756702c
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2023-05-26 18:16:28 -0500

    Weeks

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,101 @@ function App() {
         </div>
     </section>
 
+    <section className='weeks'>
+        <div className='weeks-content'>
+            <h1>Semana 1</h1>
+        </div>
+    </section>
+
+    <section className='weeks2'>
+        <div className='weeks2-content'>
+            <h1>Semana 2</h1>
+        </div>
+    </section>
+
+    <section className='weeks'>
+        <div className='weeks-content'>
+            <h1>Semana 3</h1>
+        </div>
+    </section>
+
+    <section className='weeks2'>
+        <div className='weeks2-content'>
+            <h1>Semana 4</h1>
+        </div>
+    </section>
+
+    <section className='weeks'>
+        <div className='weeks-content'>
+            <h1>Semana 5</h1>
+        </div>
+    </section>
+
+    <section className='weeks2'>
+        <div className='weeks2-content'>
+            <h1>Semana 6</h1>
+        </div>
+    </section>
+
+    <section className='weeks'>
+        <div className='weeks-content'>
+            <h1>Semana 7</h1>
+        </div>
+    </section>
+
+    <section className='weeks2'>
+        <div className='weeks2-content'>
+            <h1>Semana 8</h1>
+        </div>
+    </section>
+
+    <section className='weeks'>
+        <div className='weeks-content'>
+            <h1>Semana 9</h1>
+        </div>
+    </section>
+
+    <section className='weeks2'>
+        <div className='weeks2-content'>
+            <h1>Semana 10</h1>
+        </div>
+    </section>
+
+    <section className='weeks'>
+        <div className='weeks-content'>
+            <h1>Semana 11</h1>
+        </div>
+    </section>
+
+    <section className='weeks2'>
+        <div className='weeks2-content'>
+            <h1>Semana 12</h1>
+        </div>
+    </section>
+
+    <section className='weeks'>
+        <div className='weeks-content'>
+            <h1>Semana 13</h1>
+        </div>
+    </section>
+
+    <section className='weeks2'>
+        <div className='weeks2-content'>
+            <h1>Semana 14</h1>
+        </div>
+    </section>
+
+    <section className='weeks'>
+        <div className='weeks-content'>
+            <h1>Semana 15</h1>
+        </div>
+    </section>
+
+    <section className='weeks2'>
+        <div className='weeks2-content'>
+            <h1>Semana 16</h1>
+        </div>
+    </section>
     </>
   )
 }
